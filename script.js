865453188c3b1daeb32fb6995ee88410b286bba4
Hank <hanbing0902@theduapp.com>
2020-12-27 17:02:54 +0800

    update

--- script.js
+++ script.js
@@ -8,26 +8,25 @@ hammertime.get('pinch').set({ enable: true });
 hammertime.add(new Hammer.Pinch());
 
 hammertime.on("pinchout", function (e) {
-  var W = $("#testimg").width();
-  var H = $("#testimg").height();
-  var scale = 2;
-  var mouseX=e.center.x;//捏开点
-  var mouseY=e.center.y;
+  document.body.innerText = e.center.x + '' + e.center.y
+  // var W = $("#testimg").width();
+  // var H = $("#testimg").height();
+  // var scale = 2;
+  // var mouseX=e.center.x;//捏开点
+  // var mouseY=e.center.y;
 
-  if($("#test").attr("data-scale")==1){
-      var translateX=0;
-      var translateY=0;
-      //计算当前点击点相对于图片的偏移比例
-      var posX = mouseX/W;
-      var posY = mouseY/H;
-      translateX= (W * posX / scale) * -1;
-      translateY= (H * posY / scale) * -1;
-      
-      $("#test").css("transformOrigin","0% 0%");
-      $("#test").css("transform","scale(2,2) translate("+translateX+"px, "+translateY+"px)");
-      $("#test").attr("data-x",translateX);
-      $("#test").attr("data-y",translateY);
-      $("#test").attr("data-scale",2);
-  }
+  // var translateX=0;
+  // var translateY=0;
+  // //计算当前点击点相对于图片的偏移比例
+  // var posX = mouseX/W;
+  // var posY = mouseY/H;
+  // translateX= (W * posX / scale) * -1;
+  // translateY= (H * posY / scale) * -1;
+  
+  // $("#test").css("transformOrigin","0% 0%");
+  // $("#test").css("transform","scale(2,2) translate("+translateX+"px, "+translateY+"px)");
+  // $("#test").attr("data-x",translateX);
+  // $("#test").attr("data-y",translateY);
+  // $("#test").attr("data-scale",2);
 
 });
