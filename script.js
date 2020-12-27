e92860812fc4a0efbc3aa5d9581b7da3dd89fbfc
Hank <hanbing0902@theduapp.com>
2020-12-27 12:19:59 +0800

    update

--- script.js
+++ script.js
@@ -10,12 +10,31 @@ function autoVideo(selector) {
   }, 500)
 }
 
-autoVideo('#p-1')
-autoVideo('#p-2')
-autoVideo('#p-3')
-autoVideo('#video1')
-autoVideo('#video2')
-autoVideo('#video3')
+// autoVideo('#p-1')
+// autoVideo('#p-2')
+// autoVideo('#p-3')
+// autoVideo('#video1')
+// autoVideo('#video2')
+// autoVideo('#video3')
+
+function videoRealPlay(obj) {
+  var videoOri = document.querySelector(`#${obj.ori}`)
+  var videoReal = document.querySelector(`#${obj.real}`)
+  videoOri.addEventListener('click', function () {
+    videoReal.play()
+    videoReal.style.visibility = 'visible'
+  })
+
+  videoReal.addEventListener('click', function () {
+    videoReal.pause()
+    videoReal.style.visibility = 'hidden'
+  })
+}
+
+videoRealPlay({ori: 'p-1', real: 'p1r'})
+videoRealPlay({ori: 'p-2', real: 'p2r'})
+videoRealPlay({ori: 'p-3', real: 'p3r'})
+
 
 var header = document.querySelector('.header')
 header.addEventListener('click', function () {
