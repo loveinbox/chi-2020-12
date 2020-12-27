aac471464045243c14907d203a81dab84a3b2bf9
Hank <hanbing0902@theduapp.com>
2020-12-27 17:16:06 +0800

    update

--- script.js
+++ script.js
@@ -1,32 +1,5 @@
-// new PinchZoom(document.querySelector('#test'))
-var test = document.querySelector('#test')
+// just grab a DOM element
+var element = document.querySelector('#test')
 
-//创建一个新的hammer对象并且在初始化时指定要处理的dom元素
-var hammertime = new Hammer(test);
-//var hammertime = new Hammer(document.getElementById("test"));
-hammertime.get('pinch').set({ enable: true });
-hammertime.add(new Hammer.Pinch());
-
-hammertime.on("pinchout", function (e) {
-  document.body.innerText = e.center.x + '' + e.center.y
-  // var W = $("#testimg").width();
-  // var H = $("#testimg").height();
-  // var scale = 2;
-  // var mouseX=e.center.x;//捏开点
-  // var mouseY=e.center.y;
-
-  // var translateX=0;
-  // var translateY=0;
-  // //计算当前点击点相对于图片的偏移比例
-  // var posX = mouseX/W;
-  // var posY = mouseY/H;
-  // translateX= (W * posX / scale) * -1;
-  // translateY= (H * posY / scale) * -1;
-  
-  // $("#test").css("transformOrigin","0% 0%");
-  // $("#test").css("transform","scale(2,2) translate("+translateX+"px, "+translateY+"px)");
-  // $("#test").attr("data-x",translateX);
-  // $("#test").attr("data-y",translateY);
-  // $("#test").attr("data-scale",2);
-
-});
+// And pass it to panzoom
+panzoom(element)
